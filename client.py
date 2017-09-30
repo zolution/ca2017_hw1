ab38abf1cbd0d210976fb99671638f73c7d8cdd7
zolution <zolution@outlook.com>
2017-09-30 14:35:34 +0800

    Finish Repeat

--- client.py
+++ client.py
@@ -1,14 +1,26 @@
 import socket
 
+
+
+def sendMsg(Socket, Msg, channel):
+    Comd = "PRIVMSG " + channel + " :" + Msg + "\r\n"
+    print "--------------" + Comd
+    Socket.send(bytes(Comd))
+
+    
 if __name__ == '__main__':
     IRCSocket = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
     IRCSocket.connect( ('irc.freenode.net', 6667) )
     NICK = 'robot_077'
     USER = 'zolution testing testing Jason'
-    Msg = 'NICK ' + NICK + '\r\nUSER ' + USER + '\r\nJOIN #CN_DEMO \r\nILoveTA\r\n'
+    CHANNEL = '#CN_DEMO'
+    Msg = 'NICK ' + NICK + '\r\nUSER ' + USER + '\r\nJOIN ' + CHANNEL + '\r\n'
     IRCSocket.send(bytes(Msg))
+    sendMsg(IRCSocket, "Hello! I am robot.",CHANNEL)
 
     while True:
         IRCMsg = IRCSocket.recv(4096)
         print IRCMsg
+        if IRCMsg.find(":@repeat") != -1:
+            sendMsg(IRCSocket, IRCMsg[IRCMsg.find(":@repeat")+9:], CHANNEL)
 
