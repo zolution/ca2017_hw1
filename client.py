66075b7a5ef53ade869a67cabbc7f557ac6cc789
zolution <zolution@outlook.com>
2017-09-30 15:15:39 +0800

    Finish Convert and fix race condition of messages/echoes

--- client.py
+++ client.py
@@ -7,6 +7,15 @@ def sendMsg(Socket, Msg, channel):
     print "--------------" + Comd
     Socket.send(bytes(Comd))
 
+def Convert(Socket, Msg, channel):
+    try:
+        if Msg[:2] == '0x':
+            sendMsg(Socket, str(int(Msg,16)), channel)
+        else:
+            sendMsg(Socket, hex(int(Msg,0)), channel)
+    except:
+        sendMsg(Socket, "Invalid Input "+ Msg, channel)
+
     
 if __name__ == '__main__':
     IRCSocket = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
@@ -21,6 +30,11 @@ if __name__ == '__main__':
     while True:
         IRCMsg = IRCSocket.recv(4096)
         print IRCMsg
-        if IRCMsg.find(":@repeat") != -1:
-            sendMsg(IRCSocket, IRCMsg[IRCMsg.find(":@repeat")+9:], CHANNEL)
+        for Msg in IRCMsg.split('\r\n'):
+            if Msg.find(":@repeat") != -1:
+                sendMsg(IRCSocket, IRCMsg[IRCMsg.find(":@repeat")+9:], CHANNEL)
+            elif Msg.find(":@convert") != -1:
+                Convert(IRCSocket, IRCMsg[IRCMsg.find(":@convert")+10:], CHANNEL)
+            elif Msg.find(":@ip") != -1:
+                IP(IRCSocket, IRCMsg[IRCMsg.find(":@ip")+5:], CHANNEL)
 
